2c11e99e9514e9c1f7613fc59048a0095f39624a
Lam Ngoc Khuong <me@ngockhuong.com>
2018-01-25 10:05:20 +0700

    admin design static pages and auth part 2

--- database/factories/DatabaseFactory.php
+++ database/factories/DatabaseFactory.php
@@ -28,7 +28,7 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
+        'password' => bcrypt('123456'),
         'avatar' => $faker->imageUrl,
         'coin' => $faker->randomNumber,
     ];
