5c67707a582dc16d728a03884328a4d2e859fe8a
Lam Ngoc Khuong <me@ngockhuong.com>
2018-02-06 13:02:42 +0700

    authorization part 1

--- resources/views/vendor/light-bootstrap-dashboard/layouts/sidebar/sidebar-wrapper/main.blade.php
+++ resources/views/vendor/light-bootstrap-dashboard/layouts/sidebar/sidebar-wrapper/main.blade.php
@@ -1,20 +1,20 @@
 <div class="sidebar-wrapper">
     <div class="logo">
         @section('logo')
-        <a href="/" class="simple-text">
-            @yield('title', trans('admin.title'))
-        </a>
+            <a href="#" class="simple-text">
+                @lang('admin.title')
+            </a>
         @show
     </div>
 
     @section('sidebar-menu')
-    <ul class="nav">
-        <li class="active">
-            <a href="#">
-                <i class="pe-7s-home"></i>
-                <p>@lang('admin.sidebar_menu.home')</p>
-            </a>
-        </li>
-    </ul>
+        <ul class="nav">
+            <li class="active">
+                <a href="#">
+                    <i class="pe-7s-home"></i>
+                    <p>@lang('admin.sidebar_menu.home')</p>
+                </a>
+            </li>
+        </ul>
     @show
 </div>
